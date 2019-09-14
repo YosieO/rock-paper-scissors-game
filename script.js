4a0f7f8860dae43c7c84e93eb82dd12f53fecd74
Yosie Octavia <yosie.octavia@gmail.com>
2019-09-14 16:32:05 +0200

    :sparkle: add main game functionality

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
-{
-    alert('hi')();
+const CHOICES = ['rock', 'paper', 'scissors']
+
+let computerChoice 
+let userChoice
+let isUserWinner 
+
+const submit = function () {
+    computerChoice = CHOICES[Math.floor(Math.random() * CHOICES.length)]
+    userChoice = document.querySelector('.radio:checked').value
+    checkWinner() // call a function, which is checkWinner.
+    console.log(userChoice, computerChoice, isUserWinner)
+}
+
+const checkWinner = () => {
+    if (userChoice === 'rock' && computerChoice === 'scissors') {
+        isUserWinner = true
+    } else if (userChoice === 'paper' && computerChoice === 'rock' ) {
+        isUserWinner = true
+    } else if (userChoice === 'scissors' && computerChoice === 'paper') {
+        isUserWinner = true
+    } else if (userChoice === computerChoice) {
+        isUserWinner = false
+    } else {
+        isUserWinner = false
+    }
+    return isUserWinner
 }
